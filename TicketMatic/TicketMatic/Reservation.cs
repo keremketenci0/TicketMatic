6d2b41af06df6fe9a3adfaf4406cfabe43097c45
Kerem Ketenci <128905838+keremketenci0@users.noreply.github.com>
2023-05-15 19:28:50 +0300

    Add files via upload

---
+++ TicketMatic/TicketMatic/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using TicketMatic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+
+namespace TicketMatic
+{
+    class Reservation
+    {
+        public Session session { get; set; }
+        public Hall hall { get; set; }
+        public Movie movie { get; set; }
+        public DateTime date { get; set; }
+        public string full_name { get; set; }
+        public string seat { get; set; }
+        public string payment_method { get; set; }
+
+
+    }
+}
